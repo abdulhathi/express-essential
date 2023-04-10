861d58f6b408f51a0489a713197c778a2fffc552
Abdul Hathi Mohamed Hussain <abdulhathi@abduls-mbp.lan>
2023-04-10 02:19:47 -0700

    Node js express essential first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import StudentData from "./data/student_data.json" assert { type: "json" };
+
+const app = express();
+const PORT = 3000;
+
+// Get method
+app.get("/", (request, response) => {
+  response.send("This is the response from get method");
+});
+
+// Post method
+
+app.post("/create", (request, response) => {
+  response.send("This is the create/ post method.")
+})
+
+app.get("/student/:id", (request, response) => {
+  const stdId = Number(request.params.id);
+  const stdData = StudentData.filter((std) => std.id === stdId);
+  response.json(stdData);
+});
+
+app.listen(PORT, () => {
+  console.log(`Node js server is running in port : ${PORT}`); 
+})
